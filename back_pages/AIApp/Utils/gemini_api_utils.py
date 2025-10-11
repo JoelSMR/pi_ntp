b8a0c81776920e10f9a54abc04f5915b5093d5da
p <p@:p.com>
2025-10-10 20:03:28 -0500

    fix(ApiKey writable by user)

--- back_pages/AIApp/Utils/gemini_api_utils.py
+++ back_pages/AIApp/Utils/gemini_api_utils.py
@@ -1,11 +1,11 @@
 from google import genai
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from os import getenv
 
 
-def ask_question_to_gemini(prompt:str) -> str :
-    load_dotenv()
-    client = genai.Client(api_key=getenv("GEMINI-API-KEY"))
+def ask_question_to_gemini(prompt:str,api_key) -> str :
+    #load_dotenv()
+    client = genai.Client(api_key=api_key)
 
     response =client.models.generate_content(
         model="gemini-2.5-flash",
--- pages_components/AIApp/ask_to_gemini.py
+++ pages_components/AIApp/ask_to_gemini.py
@@ -6,9 +6,10 @@ def ask_to_gemini():
         col1,col2=st.columns(2)
         col1.title("Ask to gemini")
         question=st.text_input("Ingresa tu pregunta")
+        key= st.text_input("API KEY")
         answer="Aprenderemos Cosas Nuevas"
         if st.button("Preguntar"):
             st.spinner(text="Thinking",show_time=True)
-            answer=  gemini_api_utils.ask_question_to_gemini(question)
+            answer=  gemini_api_utils.ask_question_to_gemini(question,key)
         st.text(answer)
 
