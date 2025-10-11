0106d6ffca73ed5b0638615b6615b82a70c56379
Joel <jmarinos@cesde.net>
2025-10-10 20:32:23 -0500

    LOADING MockAPI

--- pages/5_App Consumo mockAPI.py
+++ pages/5_App Consumo mockAPI.py
@@ -3,40 +3,7 @@ import requests
 
 API_URL = "https://68dc955d7cd1948060aaba15.mockapi.io/Usuarios"
 
-st.set_page_config(page_title="Usuarios MockAPI", page_icon="👥", layout="wide")
-
-st.markdown("""
-    <style>
-        body {
-            background-color: #0e1117;
-        }
-        .user-card {
-            background-color: #1e1e1e;
-            border-radius: 12px;
-            padding: 20px;
-            margin-bottom: 15px;
-            color: white;
-            box-shadow: 0px 2px 10px rgba(0,0,0,0.3);
-            transition: transform 0.2s;
-        }
-        .user-card:hover {
-            transform: scale(1.02);
-        }
-        .user-name {
-            font-size: 1.2rem;
-            font-weight: 600;
-            color: #00b4d8;
-        }
-        .user-age {
-            font-size: 1rem;
-            color: #adb5bd;
-        }
-        .title {
-            color: #f8f9fa;
-            font-weight: 700;
-        }
-    </style>
-""", unsafe_allow_html=True)
+st.set_page_config(page_title="App Consumo mockAPI", layout="wide")
 
 def obtener_usuarios():
     try:
